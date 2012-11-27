dd1ab943a4d2824733460a8b3f24c66a8bd0380d
xirukitepe <xiruki@programmer.net>
2012-11-27 16:03:05 +0800

    MongoMapper Implementation

---
+++ app/models/expense.rb
@@ -0,0 +1,7 @@
+class Expense
+  include MongoMapper::Document
+
+  key :expense_name, String
+    key :date_spent,String
+      key :amount, Integer
+      end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExpenseRecord::Application.config.secret_token = '0df034ca4a79ff4423d41fbba147b5ad049457fd3a0d4466c72cce396edc5aabf27dd10108dc6bd78839ff928a71168a17d92dd54d53ad18402ee333439f33e8'
